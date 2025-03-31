8f51284222ff6faa38715cbfcc295c3acb7525bd
Jiří Strnadel <Juzba88@gmail.com>
2025-03-31 17:54:31 +0200

    funkční?

--- SudokuApi/Controllers/SudokuController.cs
+++ SudokuApi/Controllers/SudokuController.cs
@@ -1,83 +1,48 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SudokuApi.Controllers
 {
-    public class SudokuController : Controller
+
+    [ApiController]
+    [Route("api[controller]")]
+    public class SudokuController : ControllerBase
     {
-        //// GET: SudokuController
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        //// GET: SudokuController/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
-
-        //// GET: SudokuController/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        // POST: SudokuController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [HttpPost("sudokusolver")]
+        public IActionResult Post([FromBody] int[][][] data)
         {
+
+
             try
             {
-                return RedirectToAction(nameof(Index));
+
+                foreach (var matrix in data)
+                {
+                    foreach (var array in matrix)
+                    {
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            array[i] = 5;
+                        }
+                    }
+                }
+
+                return Ok(new { result = data });
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+
+                return StatusCode(500, new { error = ex.Message });
             }
+
+
+
+
+
+
         }
 
-        // GET: SudokuController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
 
-        // POST: SudokuController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
 
-        // GET: SudokuController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
 
-        // POST: SudokuController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
